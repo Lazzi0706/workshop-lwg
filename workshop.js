ce04d028f6657691ce118a36e18e08d0560a6bcf
Lazzi0706 <49803294+Lazzi0706@users.noreply.github.com>
2020-12-10 16:50:41 +0700

    Update workshop.js

--- workshop.js
+++ workshop.js
@@ -14,67 +14,28 @@ const workshopMenu = document.createElement('div')
 
 
 // init
+function createEl(node, text) {
+    node.style.position = 'absolute'
+    node.style.zIndex = 1;
+    node.style.width = 650 + 'px'
+    node.style.height = 'auto'
+    node.style.marginLeft = 31 + '%'
+    node.style.marginTop = 10 + '%'
+    node.style.textAlign = 'center'
+    node.style.background = '#275494'
+    node.style.border = 2 + 'px solid #6898DD'
+    node.innerHTML = text
+    $('#lobbyDiv').append(node)
+};
+
 workshopButton.innerHTML = 'Workshop!'
 workshopButton.style.marginLeft = 2 + 'px'
-
-workshopMenu.id = 'workshopMenuId'
 
 $(getMoreBlock).append(workshopButton)
 //
 workshopButton.onclick = function getMenu() {
 
-    // Хрень-меню
-
-    workshopMenu.style.position = 'absolute'
-    workshopMenu.style.zIndex = 1
-    workshopMenu.style.width = 650 + 'px'
-    workshopMenu.style.height = 'auto'
-    workshopMenu.style.marginLeft = 31 + '%'
-    workshopMenu.style.marginTop = 10 + '%'
-    workshopMenu.style.textAlign = 'center'
-    workshopMenu.style.background = '#275494'
-    workshopMenu.style.border = 2 + 'px solid #6898DD'
-    workshopMenu.innerHTML = '<h1> Workshop! </h1>'
-
-    // Хрень-блок со скриптами и говнокодом
-    const workshopBlockLazzi = document.createElement('div')
-    const workshopBlockRynus = document.createElement('div')
-    const workshopBlockSilverBoss = document.createElement('div')
-
-    const LazziButton = document.createElement('button')
-    const RynusButton = document.createElement('button')
-    const SilverButton = document.createElement('button')
-
-    const lazziLink = new Map([['Workshop', 'https://github.com/Lazzi0706/workshop-lwg/blob/main/workshop.js'], ['Language', 'https://github.com/Lazzi0706/workshop-lwg/blob/main/workshop.js']])
-    const rynusLink = new Map
-    const silverLink = new Map
-    //
-    workshopBlockLazzi.classList.add('gold_buy_div')
-    workshopBlockRynus.classList.add('gold_buy_div')
-    workshopBlockSilverBoss.classList.add('gold_buy_div')
-
-// Test part
-    LazziButton.innerHTML = "Lazzi"
-    LazziButton.onclick = () => {
-
-        for (let [value,key] of lazziLink) {
-
-            const a = document.createElement('div'); 
-            a.style.background = 'cyan';
-            a.innerHTML = "<a>" + value + "</a>"
-            a.onclick = () => window.open(key);
-            $('#workshopMenuId').append(a)
-            console.log(value)
-
-        };
-        
-    }
-
-    RynusButton.innerHTML = "Rynus"
-    RynusButton.onclick
-
-    SilverButton.innerHTML = "Silverboss"
-    SilverButton.onclick
+   createEl(workshopMenu,'<h1>Workshop!</h1>')
 
     // Close button
     workshopClose.innerHTML = 'Close'
@@ -82,17 +43,6 @@ workshopButton.onclick = function getMenu() {
     workshopClose.style.marginLeft = 90 + '%'
     workshopClose.onclick = () => workshopMenu.remove()
 
-    // Show elements
-
-    $(workshopMenu).append(workshopBlockLazzi)
-    $(workshopMenu).append(workshopBlockRynus)
-    $(workshopMenu).append(workshopBlockSilverBoss)
-
-    $(workshopBlockLazzi).append(LazziButton)
-    $(workshopBlockRynus).append(RynusButton)
-    $(workshopBlockSilverBoss).append(SilverButton)
-
     $(workshopMenu).append(workshopClose)
 
-    $('#lobbyDiv').append(workshopMenu)
 }
